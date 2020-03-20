59d4eb86040745da59709349d3a2294c2f63b4f5
tongmi <3343977167@qq.com>
2020-03-20 20:16:56 +0800

    1

--- expression.cpp
+++ expression.cpp
@@ -1,12 +1,18 @@
 // file's name : expression.c   Tomi解释器
 #include <iostream>
-//#include <csignal>
+#include <cstring>
 #include <unistd.h>
 #include <fstream> //   getline(test,command_buf);
 #include <string> //string str; getline(cin,str); data()函数返回指向自己的第一个字符的指针.  const char *str = str2.c_str();  //要加const，否则报错
 using namespace std;
+int command_expression(string* buf,int* ret);//语法分析器
+
+
 int command_expression(string* buf,int* ret)
 {
     *ret=1;
+    const char *a=buf->data();
+    printf("%d    %s\n",strlen(a),a);
+
     return 1;
 }
