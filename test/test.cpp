fae58a5398e36c18029b4fa0614c946179327fa4
tongmi <3343977167@qq.com>
2020-03-23 13:22:25 +0800

    1

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 int main()
 {
-    string str[3];
-    for (int i = 0; i < 4; i++)
+    string command_buf;
+    getline(cin,command_buf);
+    if(command_buf=="")
     {
-        cin>>str[0];
+        cout<<"滚"<<endl;
+        exit(-1);
     }
-    for (int i = 0; i < 4; i++)
+    const char *com=command_buf.data();
+    char str[strlen(com)+1];
+    strcpy(str,com);
+    for (size_t i = 0; 1; i++)
     {
-        cout<<str[0]<<endl;
+        if(str[i]=='\0')
+        {
+            cout <<"结束"<<endl;
+            return 0;
+        }
+        if(str[i]==' ')
+        {
+            cout <<"发现空格"<<endl;
+        }
     }
+    
+    
 }
