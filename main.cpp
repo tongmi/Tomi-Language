1d3bb28a82c147169529e35c23479cee3f79643b
tongmi <3343977167@qq.com>
2020-03-30 11:40:38 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,1 @@
-extern int main_tomi(int,char**) throw();
-int main(int argc,char *argv[])
-{
-    return main_tomi(argc,argv);
-}
+extern int main_tomi(int,char**) throw();int main(int argc,char *argv[]){return main_tomi(argc,argv);}
